5dc309f17f1209a1cf9b71ca63b403097575ae6b
ztrunk <zabbix@ztrunk.space>
2026-09-11 16:34:01 +0200

    chore: Update .gitignore and enhance dotenv loading in settings
    - Added .neon and .env.local to .gitignore to prevent sensitive files from being tracked.
    - Modified settings.py to load environment variables from .env and .env.local, allowing for better configuration management.

--- settings/settings.py
+++ settings/settings.py
@@ -3,10 +3,11 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env.local", override=True)
 
 
 # Quick-start development settings - unsuitable for production
